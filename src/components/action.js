5ae70cb9357eea7f297959809e0b203afa92b48b
Jason Lam <jaylam111@gmail.com>
2017-10-04 19:21:31 -0400

    housekeeping

--- src/components/action.js
+++ src/components/action.js
@@ -1,36 +1,5 @@
 import React, { Component } from 'react';
 
-// class Action extends Component {
-
-// 	// handleOnChooser() {
-// 	// 	// SETUP
-// 	// 	const options = this.props.options
-// 	// 	console.log('this.props FROM OPTIONS : ', this.props)
-// 	// 	// const randomNum = Math.floor(Math.random() * 10)
-// 	// 	const randomOptNum = Math.floor(Math.random() * options.length)
-// 	// 	const randomOpt = options[randomOptNum]
-// 	// 	alert(randomOpt)
-// 	// }
-
-
-
-// 	render() {
-// 		let props = this.props;
-
-// 		return (
-// 			<div>
-// 				<button
-// 					disabled={props.hasOptions}
-// 					onClick={props.handleChooser}
-// 				>Choose for me!
-// 				</button>
-// 			</div>
-// 		);
-// 	}
-// }
-
-// export default Action;
-
 const Action = (props) => {
 	return (
 		<div>
