0cca39f4cef28eb52c349f5b70ea2003bfe79253
Jason Lam <jaylam111@gmail.com>
2017-10-04 19:20:29 -0400

    action now stateless

--- src/components/action.js
+++ src/components/action.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 
-class Action extends Component {
+// class Action extends Component {
 
-	// handleOnChooser() {
-	// 	// SETUP
-	// 	const options = this.props.options
-	// 	console.log('this.props FROM OPTIONS : ', this.props)
-	// 	// const randomNum = Math.floor(Math.random() * 10)
-	// 	const randomOptNum = Math.floor(Math.random() * options.length)
-	// 	const randomOpt = options[randomOptNum]
-	// 	alert(randomOpt)
-	// }
+// 	// handleOnChooser() {
+// 	// 	// SETUP
+// 	// 	const options = this.props.options
+// 	// 	console.log('this.props FROM OPTIONS : ', this.props)
+// 	// 	// const randomNum = Math.floor(Math.random() * 10)
+// 	// 	const randomOptNum = Math.floor(Math.random() * options.length)
+// 	// 	const randomOpt = options[randomOptNum]
+// 	// 	alert(randomOpt)
+// 	// }
 
 
 
-	render() {
-		let props = this.props;
+// 	render() {
+// 		let props = this.props;
 
-		return (
-			<div>
-				<button
-					disabled={props.hasOptions}
-					onClick={props.handleChooser}
-				>Choose for me!
-				</button>
-			</div>
-		);
-	}
+// 		return (
+// 			<div>
+// 				<button
+// 					disabled={props.hasOptions}
+// 					onClick={props.handleChooser}
+// 				>Choose for me!
+// 				</button>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default Action;
+
+const Action = (props) => {
+	return (
+		<div>
+			<button
+				disabled={props.hasOptions}
+				onClick={props.handleChooser}
+			>Choose for me!
+			</button>
+		</div>
+	)
 }
 
 export default Action;
