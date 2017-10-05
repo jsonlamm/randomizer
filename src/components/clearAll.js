6ef35fc03e502ee7fa55f6f752829fcfb74e56fe
Jason Lam <jaylam111@gmail.com>
2017-10-04 20:10:08 -0400

    clearall now stateless

--- src/components/clearAll.js
+++ src/components/clearAll.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 
-class ClearAll extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div>
-				<button
-					disabled={this.props.hasNoOptions}
-					onClick={this.props.handleClearAll}
-				>Clear All</button>
-			</div>
-		);
-	}
+const ClearAll = (props) => {
+	return (
+		<div>
+			<button
+				disabled={props.hasNoOptions}
+				onClick={props.handleClearAll}
+			>Clear All
+			</button>
+		</div>
+	)
 }
 
 export default ClearAll;
